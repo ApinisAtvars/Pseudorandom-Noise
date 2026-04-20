e1c43e877b10e497f5bc4ba90a4cc995a836634f
ApinisAtvars <atvars.apinis@student.howest.be>
2026-04-20 17:55:20 -0400

    DONE: 1. Visualization

---
+++ Assets/Scripts/HashVisualization.cs
@@ -0,0 +1,87 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+using static Unity.Mathematics.math;
+
+public class HashVisualization : MonoBehaviour {
+
+	[BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+	struct HashJob : IJobFor {
+
+		[WriteOnly]
+		public NativeArray<uint> hashes;
+
+		public int resolution;
+
+		public float invResolution;
+        
+		public void Execute(int i) {
+			float v = floor(invResolution * i + 0.00001f);
+			float u = i - resolution * v;
+			// Pass u and v in order to have the pattern independent of the resolution
+			hashes[i] = (uint)(frac(u * v * 0.381f) * 256f); // Weyl sequence as in Organic Variety fractal tutorial
+		}
+	}
+
+    private static int
+		hashesId = Shader.PropertyToID("_Hashes"),
+		configId = Shader.PropertyToID("_Config");
+
+	[SerializeField]
+	private Mesh instanceMesh;
+
+	[SerializeField]
+	private Material material;
+
+	[SerializeField, Range(1, 512)]
+	private int resolution = 16;
+
+	private NativeArray<uint> hashes;
+
+	private ComputeBuffer hashesBuffer;
+
+	private MaterialPropertyBlock propertyBlock;
+
+	
+
+    void OnEnable () {
+		int length = resolution * resolution;
+		hashes = new NativeArray<uint>(length, Allocator.Persistent);
+		hashesBuffer = new ComputeBuffer(length, 4);
+
+		new HashJob {
+			hashes = hashes,
+			resolution = resolution,
+			invResolution = 1f / resolution
+		}.ScheduleParallel(hashes.Length, resolution, default).Complete();
+
+		hashesBuffer.SetData(hashes);
+
+		propertyBlock ??= new MaterialPropertyBlock();
+		propertyBlock.SetBuffer(hashesId, hashesBuffer);
+		propertyBlock.SetVector(configId, new Vector4(resolution, 1f / resolution));
+	}
+
+    void OnDisable () {
+		hashes.Dispose();
+		hashesBuffer.Release();
+		hashesBuffer = null;
+	}
+
+	void OnValidate () {
+		if (hashesBuffer != null && enabled) {
+			OnDisable();
+			OnEnable();
+		}
+	}
+
+    void Update () {
+		Graphics.DrawMeshInstancedProcedural(
+			instanceMesh, 0, material, new Bounds(Vector3.zero, Vector3.one),
+			hashes.Length, propertyBlock
+		);
+	}
+}
---
+++ Assets/Shaders/HashVisualization.hlsl
@@ -0,0 +1,50 @@
+#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+	StructuredBuffer<uint> _Hashes;
+#endif
+
+float4 _Config;
+
+void ConfigureProcedural () {
+	#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+		// Convert 1D line into 2D grid.
+		// We do this by dividing the identifier by the resolution as an int division
+		// GPUs don't have int division, so we just floor the result to drop the frac part
+		// As a result, we get the coordinate in the 2nd dimension
+		// Add a slight positive bias as otherwise floor might return values that are not whole
+		float v = floor(_Config.y * unity_InstanceID + 0.00001);
+		// Using the 2nd dimension coordinate, we can get the 1st dimension coordinate
+		// _Config.x is the resolution
+		float u = unity_InstanceID - _Config.x * v;
+		
+		
+		unity_ObjectToWorld = 0.0;
+		unity_ObjectToWorld._m03_m13_m23_m33 = float4(
+			_Config.y * (u + 0.5) - 0.5,
+			0.0,
+			_Config.y * (v + 0.5) - 0.5,
+			1.0
+		);
+		unity_ObjectToWorld._m00_m11_m22 = _Config.y;
+	#endif
+}
+
+
+// Get the hash, and use it to produce an RGB color.
+float3 GetHashColor () {
+	#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+		uint hash = _Hashes[unity_InstanceID];
+		return (1.0 / 255.0) * (hash & 255); // Make pattern repeat every 255 blocks
+	#else
+		return 1.0;
+	#endif
+}
+
+void ShaderGraphFunction_float (float3 In, out float3 Out, out float3 Color) {
+	Out = In;
+	Color = GetHashColor();
+}
+
+void ShaderGraphFunction_half (half3 In, out half3 Out, out half3 Color) {
+	Out = In;
+	Color = GetHashColor();
+}
